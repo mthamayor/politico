096c243227958e028a41f7ee375a17d1e1b65d58
Anifowose Habeeb <anifowosehabeeb@gmail.com>
2019-01-29 12:55:16 +0100

    [Finishes #163500814] Server side setup

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/index';
 
-
+// Initialize express
 const app = express();
 
+// Configure the middleware to accept both JSON and URL encoded data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Module used to route the files
 app.use(router);
 
 const PORT = 5000;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,5 @@
 import express from 'express';
-import dbHelper from '../db/dbHelper';
-
-
+// Initialize the router
 const router = express.Router();
-
-router.post('/api/v1/parties', dbHelper.createParty);
 
 export default router;
