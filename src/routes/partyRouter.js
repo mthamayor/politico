cf6ca2a7e6bce0f880041f0cf86bb00948f6a400
Anifowose Habeeb <anifowosehabeeb@gmail.com>
2019-01-31 15:51:12 +0100

    [Delivers

--- src/routes/partyRouter.js
+++ src/routes/partyRouter.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import PartyController from '../controllers/PartyController';
-import createPartyMiddleWare from '../middlewares/createPartyMiddleware';
 import editPartyMiddleWare from '../middlewares/editPartyMiddleware';
 
 const partyRouter = express.Router();
 
-partyRouter.post('/', createPartyMiddleWare, PartyController.createParty);
 partyRouter.patch('/:id', editPartyMiddleWare, PartyController.editPoliticalParty);
 
 export default partyRouter;
