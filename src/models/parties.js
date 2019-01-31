14d62d3032e91aa322a1d00d03e743b16a1df378
Anifowose Habeeb <anifowosehabeeb@gmail.com>
2019-01-31 08:44:01 +0100

    fixed some bugs

--- src/models/parties.js
+++ src/models/parties.js
@@ -1,4 +1,8 @@
 // Create my dummy parties
-const parties = [];
+const parties = [
+  {
+    id: 0, name: 'ADC', hqAddress: 'Ganmo', logoUrl: 'ADC logo url',
+  },
+];
 
 export default parties;
--- src/routes/partyRouter.js
+++ src/routes/partyRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import PartyController from '../controllers/PartyController';
 import createPartyMiddleWare from '../middlewares/createPartyMiddleware';
+import editPartyMiddleWare from '../middlewares/editPartyMiddleware';
 
 const partyRouter = express.Router();
 
 partyRouter.post('/', createPartyMiddleWare, PartyController.createParty);
+partyRouter.patch('/:id', editPartyMiddleWare, PartyController.editPoliticalParty);
 
 export default partyRouter;
